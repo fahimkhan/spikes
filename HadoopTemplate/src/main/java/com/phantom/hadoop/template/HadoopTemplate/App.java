753313b06c821a07a174100860793aa769d9c33b
fahimkhan <khanfahim2000@yahoo.com>
2016-08-09 16:25:59 +0530

    Hadoop Template added

---
+++ HadoopTemplate/src/main/java/com/phantom/hadoop/template/HadoopTemplate/App.java
@@ -0,0 +1,13 @@
+package com.phantom.hadoop.template.HadoopTemplate;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
